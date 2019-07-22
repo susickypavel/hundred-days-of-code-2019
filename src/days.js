4a64c6abc539080bc0ca5b10f3410f28273c08bd
Pavel Sušický <pavels.sus@gmail.com>
2019-07-22 22:36:28 +0200

    add day 16

--- src/days.js
+++ src/days.js
@@ -13,8 +13,8 @@ import Day9 from "./days/009"
 import Day10 from "./days/010"
 import Day11 from "./days/011"
 import Day12 from "./days/012"
-
-export const days = [Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10, Day11, Day12]
+import Day16 from "./days/016"
+export const days = [Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10, Day11, Day12, Day16]
 
 function Days() {
   return days.map((day, index) => {
---
+++ src/days/016/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Button from "avocode-ui/lib/components/button"
+
+const Day16 = () => {
+  return <Button>H</Button>
+}
+
+export default Day16
